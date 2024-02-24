56de04b539af1503f80e5d962f3e5fb5d90d9f9a
Yash Bhappa <ybhappa@gmail.com>
2024-02-24 16:53:21 +0530

    Fructus App - SUI2 added

---
+++ Fructus-App/Fructus/Fructus/App/FructusApp.swift
@@ -0,0 +1,20 @@
+//
+//  Created by Yash Bhappa
+//
+
+import SwiftUI 
+
+@main
+struct FructusApp: App {
+  @AppStorage("isOnboarding") var isOnboarding: Bool = true
+  
+  var body: some Scene {
+    WindowGroup {
+      if isOnboarding {
+        OnboardingView()
+      } else {
+        ContentView()
+      }
+    }
+  }
+}
