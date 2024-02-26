68fdb6166328595a595e817507d2412d8b5cd1ae
Yash Bhappa <ybhappa@gmail.com>
2024-02-26 15:15:17 +0530

    Avocados app added

---
+++ Avocados/Avocados/Model/RipeningModel.swift
@@ -0,0 +1,17 @@
+//
+//  Created by Yash Bhappa
+//
+
+import SwiftUI
+
+// MARK: RIPENING MODEL
+
+struct Ripening: Identifiable {
+  var id = UUID()
+  var image: String
+  var stage: String
+  var title: String
+  var description: String
+  var ripeness: String
+  var instruction: String
+}
