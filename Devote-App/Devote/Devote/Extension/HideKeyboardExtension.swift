8bddad1dc007c91551db3009dfc44de671c4e952
Yash Bhappa <ybhappa@gmail.com>
2024-02-26 15:19:02 +0530

    Devote -SUI2 app added

---
+++ Devote-App/Devote/Devote/Extension/HideKeyboardExtension.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Yash Bhappa
+//
+
+import SwiftUI
+
+#if canImport(UIKit)
+extension View {
+  func hideKeyboard() {
+    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+  }
+}
+#endif
