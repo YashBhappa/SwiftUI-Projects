2fbc3204c27a8838ca7dd1e1336bc25ce37ab4c9
Yash Bhappa <ybhappa@gmail.com>
2024-02-26 15:14:11 +0530

    Dev app added

---
+++ Learn-by-Doing/Learn by Doing/CardModel.swift
@@ -0,0 +1,17 @@
+//
+//  Created by Yash Bhappa
+//
+
+import SwiftUI
+
+// MARK: - CARD MODEL
+
+struct Card: Identifiable {
+  var id = UUID()
+  var title: String
+  var headline: String
+  var imageName: String
+  var callToAction: String
+  var message: String
+  var gradientColors: [Color]
+}
