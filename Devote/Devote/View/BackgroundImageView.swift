fb54393cc5ed4340b12044a4ed057f3967609107
Yash Bhappa <ybhappa@gmail.com>
2024-02-26 15:18:43 +0530

    Devote app added

---
+++ Devote/Devote/View/BackgroundImageView.swift
@@ -0,0 +1,21 @@
+//
+//  Created by Yash Bhappa
+//
+
+import SwiftUI
+
+struct BackgroundImageView: View {
+  var body: some View {
+    Image("rocket")
+      .antialiased(true)
+      .resizable()
+      .scaledToFill()
+      .ignoresSafeArea(.all)
+  }
+}
+
+struct BackgroundImageView_Previews: PreviewProvider {
+  static var previews: some View {
+    BackgroundImageView()
+  }
+}
